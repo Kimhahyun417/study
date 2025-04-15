6528525432526cbc61c21d9cbd16a08096dcb6d4
Kimhahyun417 <143933699+Kimhahyun417@users.noreply.github.com>
2025-04-15 17:35:07 +0900

    1

---
+++ al/al4/al4/PriorityQueue.cpp
@@ -0,0 +1,48 @@
+#include "PriorityQueue.h"
+
+bool PriorityQueue::IsEmpty()
+{
+	return false;
+}
+
+void PriorityQueue::Push(int _priority, int _data)
+{
+}
+
+Node PriorityQueue::Pop()
+{
+	return Node();
+}
+
+int PriorityQueue::Count()
+{
+	return 0;
+}
+
+int PriorityQueue::GetChildIndex(int selfIndex)
+{
+	return 0;
+}
+
+int PriorityQueue::GetParentIndex(int selfIndex)
+{
+	return 0;
+}
+
+int PriorityQueue::GetLeftChildIndex(int selfIndex)
+{
+	return 0;
+}
+
+int PriorityQueue::GetRightChildIndex(int selfIndex)
+{
+	return 0;
+}
+
+void PriorityQueue::Swap(int firstIndex, int secondIndex)
+{
+}
+
+PriorityQueue::PriorityQueue()
+{
+}
